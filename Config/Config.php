137576387e26f68d5c109e6d60000766b0f70a7c
HugoZak <zakrajsekhugo@gmail.com>
2024-04-03 11:18:12 +0200

    🔧 Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -8,4 +8,6 @@ class Config  {
     public static $cash = 20580;
     public static $houses = 32;
     public static $hotels = 12;
+    public static $cases = 40;
+    public static $salary = 200;
 }
